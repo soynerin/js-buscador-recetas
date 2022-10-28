ac73c4679333ce6df796e75585289b3d43722630
Neri Espinola <neri.agustin.es@outlook.com>
2022-10-28 16:10:04 -0300

    feat: se muestran las categorías en el Combo de Categorías

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 function initApp() {
+
+    selectCategorias = document.getElementById('categorias');
+
     obtenerCategoriasRecetas();
 
     function obtenerCategoriasRecetas() {
         fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
             .then(respuesta => respuesta.json())
-            .then(resultado => console.log(resultado));
+            .then(resultado => mostrarCategoriasRecetas(resultado.categories));
+    }
+
+    function mostrarCategoriasRecetas(categorias = []) {
+
+        categorias.forEach(categoria => {
+            const { strCategory } = categoria;
+            const option = document.createElement('option');
+            option.value = strCategory;
+            option.textContent = strCategory;
+            selectCategorias.appendChild(option);
+        });
     }
 }
 
