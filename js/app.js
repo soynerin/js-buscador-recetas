4b3c6baabdbda6a3b29e3011a3066180f52363a7
Neri Espinola <neri.agustin.es@outlook.com>
2022-11-06 10:27:20 -0300

    feat: se muestran los resultados de las recetas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function initApp() {
 
-    selectCategorias = document.getElementById('categorias');
+    const selectCategorias = document.getElementById('categorias');
+    selectCategorias.addEventListener('change', seleccionarCategoriaReceta);
 
     obtenerCategoriasRecetas();
 
@@ -18,6 +19,36 @@ function initApp() {
             option.value = strCategory;
             option.textContent = strCategory;
             selectCategorias.appendChild(option);
+        });
+    }
+
+    function seleccionarCategoriaReceta(e) {
+        e.preventDefault();
+        const categoria = e.target.value;
+        obtenerRecetasPorCategoria(categoria);
+    }
+
+    function obtenerRecetasPorCategoria(categoria) {
+        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`)
+            .then(respuesta => respuesta.json())
+            .then(resultado => mostrarRecetasPorCategoria(resultado.meals));
+    }
+
+    function mostrarRecetasPorCategoria(recetas = []) {
+        recetas.forEach(receta => {
+            const { idMeal, strMeal, strMealThumb } = receta;
+            const div = document.createElement('div');
+            div.classList.add('col-md-4');
+            div.innerHTML = `
+                <div class="card mb-4">
+                    <img src="${strMealThumb}" alt="${strMeal}" class="card-img-top">
+                    <div class="card-body">
+                        <h3 class="card-title text-center mb-3">${strMeal}</h3>
+                        <a href="#" class="btn btn-primary w-100" data-id="${idMeal}">Ver Receta</a>
+                    </div>
+                </div>
+            `;
+            document.querySelector('#recetas').appendChild(div);
         });
     }
 }
